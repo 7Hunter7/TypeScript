36bc8ef2dd8898738bc19115222ecfaa83152bd0
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-20 10:05:52 +0300

    Updated 'task_4' in 'work_3'

--- work_3/task_4.ts
+++ work_3/task_4.ts
@@ -56,3 +56,86 @@ console.log(- ${person.name}, ${person.age}, ${additionalInformation});
 
 persons.forEach(logPerson);
 */
+
+// Решение:
+type User = {
+  name: string;
+  age: number;
+  occupation: string;
+};
+
+type Admin = {
+  name: string;
+  age: number;
+  role: string;
+};
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+  {
+    name: "Max Mustermann",
+    age: 25,
+    occupation: "Chimney sweep",
+  },
+  {
+    name: "Jane Doe",
+    age: 32,
+    role: "Administrator",
+  },
+  {
+    name: "Kate Müller",
+    age: 23,
+    occupation: "Astronaut",
+  },
+  {
+    name: "Bruce Willis",
+    age: 64,
+    role: "World saver",
+  },
+];
+
+// Тайпгард для Admin
+function isAdmin(person: Person): person is Admin {
+  return "role" in person;
+}
+
+export function logPerson(person: Person) {
+  let additionalInformation: string;
+  if (isAdmin(person)) {
+    additionalInformation = person.role;
+  } else {
+    additionalInformation = person.occupation;
+  }
+  console.log(`- ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+
+persons.forEach(logPerson);
+
+// Объяснения к коду:
+/*
+1) isAdmin(person: Person): person is Admin: 
+- Создается пользовательский тайпгард isAdmin, который проверяет наличие свойства role в объекте person. 
+- Если свойство role присутствует, функция возвращает true и сообщает TypeScript, что person имеет тип Admin в области видимости, где isAdmin возвращает true.
+
+2) logPerson(person: Person):
+- Использует тайпгард isAdmin для проверки, является ли person типом Admin.
+- Если isAdmin(person) возвращает true, то additionalInformation присваивается значение свойства role.
+- В противном случае (если person является User), additionalInformation присваивается значение свойства occupation.
+- Выводит информацию о человеке в консоль.
+*/
+
+// Сопроводительный текст:
+/*
+1) Типобезопасность: 
+Использование тайпгарда isAdmin гарантирует, что доступ к свойству role будет осуществляться только тогда, когда person действительно является типом Admin, что предотвращает потенциальные ошибки.
+
+2) Читаемость: 
+Код стал более читаемым благодаря использованию функции isAdmin, которая явно определяет, что делает проверка.
+
+3) Расширяемость: 
+Если в будущем потребуется добавить другие типы людей (например, “SuperAdmin” или “Guest”), можно легко расширить тайпгард isAdmin или создать новые тайпгарды для этих типов.
+
+4) Избежание ошибок: 
+Предыдущий код полагался на то, что если есть поле role, то это Admin, а если нет - User. Это может привести к ошибкам, если появится новый тип, который не является Admin, но при этом не имеет поля role. Использование тайпгарда делает код более надежным.
+*/
