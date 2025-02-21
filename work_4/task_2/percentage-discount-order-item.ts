d0cc993fdb20ef1e94e9e855ce4fce4e184ce34c
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-21 10:19:02 +0300

    Added 'percentage-discount-order-item.ts' in 'task_2' to 'work_4'

---
+++ work_4/task_2/percentage-discount-order-item.ts
@@ -0,0 +1,50 @@
+import { OrderItem } from "./order-item";
+import { Product } from "./product";
+
+export class PercentageDiscountOrderItem extends OrderItem {
+  private _discountPercentage: number; // Величина процентной скидки
+  private _minQuantityForDiscount: number; // Минимальное количество товара для применения скидки
+
+  constructor(
+    product: Product,
+    quantity: number,
+    discountPercentage: number,
+    minQuantityForDiscount: number
+  ) {
+    super(product, quantity);
+    this._discountPercentage = discountPercentage;
+    this._minQuantityForDiscount = minQuantityForDiscount;
+  }
+
+  get discountPercentage(): number {
+    return this._discountPercentage;
+  }
+
+  set discountPercentage(value: number) {
+    this._discountPercentage = value;
+  }
+
+  get minQuantityForDiscount(): number {
+    return this._minQuantityForDiscount;
+  }
+
+  set minQuantityForDiscount(value: number) {
+    this._minQuantityForDiscount = value;
+  }
+
+  // Метод рассчета стоимости товара с учетом процентной скидки и минимального количества
+  getCost(): number {
+    let cost = this.product.price * this.quantity;
+    if (this.quantity >= this.minQuantityForDiscount) {
+      cost = cost * (1 - this.discountPercentage / 100); // Процентная скидка
+    }
+    return Math.floor(cost); // Итоговая стоимость, округленная в меньшую сторону до целого числа.
+  }
+
+  log(): void {
+    super.log();
+    console.log(
+      `Процентная скидка: ${this.discountPercentage}%, Минимальное количество для получения скидки: ${this.minQuantityForDiscount}`
+    );
+  }
+}
